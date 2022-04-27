43429e6272d10ce328c87d32b66fabffa1e35d11
JT Archie <jtarchie@gmail.com>
2022-04-27 10:49:52 -0600

    refactor to object

--- main.go
+++ main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/alecthomas/chroma"
-	"github.com/alecthomas/chroma/formatters"
-	chromaHTML "github.com/alecthomas/chroma/formatters/html"
-	"github.com/alecthomas/chroma/quick"
 	"github.com/alexflint/go-arg"
-	"github.com/bmatcuk/doublestar/v4"
-	"github.com/gomarkdown/markdown"
-	"github.com/gomarkdown/markdown/ast"
-	"github.com/gomarkdown/markdown/html"
 	"github.com/jtarchie/notes/templates"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/spf13/afero"
 )
 
 //go:generate qtc -dir=templates
@@ -32,42 +18,9 @@ var args struct {
 }
 
 func main() {
-	chroma.StandardTypes[chroma.PreWrapper] = "highlight"
-
 	err := execute()
 	if err != nil {
 		log.Fatalf("could not execute: %s", err)
-	}
-}
-
-func trimPath(path string, trim string) string {
-	s := strings.TrimPrefix(path, trim)
-	s = strings.TrimSuffix(s, filepath.Ext(s))
-	return "/" + s
-}
-
-var _ = formatters.Register(
-	"html",
-	chromaHTML.New(
-		chromaHTML.Standalone(false),
-		chromaHTML.WithClasses(false),
-	),
-)
-
-func renderHookDropCodeBlock(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
-	switch n := node.(type) {
-	case *ast.CodeBlock:
-		source := string(n.Literal)
-		_ = quick.Highlight(
-			w,
-			source,
-			string(n.Info),
-			"html",
-			"solarized-dark256",
-		)
-		return ast.GoToNext, true
-	default:
-		return ast.GoToNext, false
 	}
 }
 
@@ -77,50 +30,15 @@ func execute() error {
 		return fmt.Errorf("could not parse arguments: %w", err)
 	}
 
-	originalDirectory, err := filepath.Abs(args.Directory)
+	renderer, err := NewRenderer(args.Directory, templates.Render)
 	if err != nil {
-		return fmt.Errorf("could not evaluate directory %q: %w", args.Directory, err)
-	}
-	log.Printf("using base directory %q\n", originalDirectory)
-
-	markdownFiles, err := doublestar.Glob(os.DirFS(originalDirectory), "**/*.md")
-	if err != nil {
-		return fmt.Errorf("could not find markdown files in %q: %w", originalDirectory, err)
-	}
-	log.Printf("found %d files with glob '**/*.md", len(markdownFiles))
-
-	opts := html.RendererOptions{
-		Flags:          html.CommonFlags,
-		RenderNodeHook: renderHookDropCodeBlock,
-	}
-	renderer := html.NewRenderer(opts)
-	memFS := afero.NewMemMapFs()
-
-	for _, markdownFile := range markdownFiles {
-		contents, err := os.ReadFile(markdownFile)
-		if err != nil {
-			return fmt.Errorf("could not read markdown file %q: %w", markdownFile, err)
-		}
-
-		strippedPath := trimPath(markdownFile, originalDirectory)
-		log.Printf("creating HTML of file %q\n", strippedPath)
-
-		html := markdown.ToHTML(contents, nil, renderer)
-		err = afero.WriteFile(
-			memFS,
-			strippedPath,
-			[]byte(templates.Render(html)),
-			os.ModePerm,
-		)
-		if err != nil {
-			return fmt.Errorf("could not setup HTML for %q: %w", strippedPath, err)
-		}
+		return fmt.Errorf("could not create renderer: %w", err)
 	}
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	e.GET("/*", echo.WrapHandler(http.FileServer(afero.NewHttpFs(memFS))))
+	e.GET("/*", echo.WrapHandler(renderer))
 
 	err = e.Start(fmt.Sprintf(":%d", args.Port))
 	if err != nil {
---
+++ renderer.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/alecthomas/chroma"
+	"github.com/alecthomas/chroma/formatters"
+	chromaHTML "github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/quick"
+	"github.com/bmatcuk/doublestar/v4"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+	mdHTML "github.com/gomarkdown/markdown/html"
+	"github.com/spf13/afero"
+)
+
+type renderer struct {
+	directory string
+	fs        afero.Fs
+	layout    func([]byte) string
+	server    http.Handler
+}
+
+var _ = formatters.Register(
+	"html-custom",
+	chromaHTML.New(
+		chromaHTML.Standalone(false),
+		chromaHTML.WithClasses(false),
+	),
+)
+
+func NewRenderer(
+	directory string,
+	layout func([]byte) string,
+) (*renderer, error) {
+	absDirectory, err := filepath.Abs(directory)
+	if err != nil {
+		return nil, fmt.Errorf("could not evaluate directory %q: %w", directory, err)
+	}
+
+	chroma.StandardTypes[chroma.PreWrapper] = "highlight"
+
+	fs := afero.NewMemMapFs()
+
+	r := &renderer{
+		directory: absDirectory,
+		fs:        fs,
+		layout:    layout,
+		server:    http.FileServer(afero.NewHttpFs(fs)),
+	}
+
+	err = r.process()
+	if err != nil {
+		return nil, fmt.Errorf("could not process markdown files: %w", err)
+	}
+
+	return r, nil
+}
+
+func (r *renderer) process() error {
+	opts := mdHTML.RendererOptions{
+		Flags:          mdHTML.CommonFlags,
+		RenderNodeHook: r.renderMarkdownHook,
+	}
+	mdRenderer := mdHTML.NewRenderer(opts)
+
+	markdownFiles, err := doublestar.Glob(os.DirFS(r.directory), "**/*.md")
+	if err != nil {
+		return fmt.Errorf("could not find markdown files in %q: %w", r.directory, err)
+	}
+
+	for _, markdownFile := range markdownFiles {
+		contents, err := os.ReadFile(markdownFile)
+		if err != nil {
+			return fmt.Errorf("could not read markdown file %q: %w", markdownFile, err)
+		}
+
+		strippedPath := r.trimPath(markdownFile, r.directory)
+
+		html := markdown.ToHTML(contents, nil, mdRenderer)
+		err = afero.WriteFile(
+			r.fs,
+			strippedPath,
+			[]byte(r.layout(html)),
+			os.ModePerm,
+		)
+		if err != nil {
+			return fmt.Errorf("could not setup HTML for %q: %w", strippedPath, err)
+		}
+	}
+
+	return nil
+}
+
+func (n *renderer) renderMarkdownHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+	switch n := node.(type) {
+	case *ast.CodeBlock:
+		source := string(n.Literal)
+		_ = quick.Highlight(
+			w,
+			source,
+			string(n.Info),
+			"html-custom",
+			"solarized-dark256",
+		)
+		return ast.GoToNext, true
+	default:
+		return ast.GoToNext, false
+	}
+}
+
+func (r *renderer) trimPath(path string, trim string) string {
+	s := strings.TrimPrefix(path, trim)
+	s = strings.TrimSuffix(s, filepath.Ext(s))
+	return "/" + s
+}
+
+func (r *renderer) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	r.server.ServeHTTP(response, request)
+}
+
+var _ http.Handler = &renderer{}
